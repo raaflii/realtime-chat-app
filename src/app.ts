ac056bcde4248309cff6ae2afcb46d11ca2e43ba
raaflii <rafliwrzky@gmail.com>
2025-08-04 16:03:29 +0700

    add connection to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from "express";
-import cors from "cors"
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { connectDB } from "./config/db.ts";
+
+connectDB();
 
 const app = express();
-
-app.use(cors());
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
   console.log("Response sent");
 });
 
-export default app
+export default app;

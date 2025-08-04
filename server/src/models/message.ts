5084fd521173e00fdf06f0aa278e5705081761fc
raaflii <rafliwrzky@gmail.com>
2025-08-04 23:59:15 +0700

    add new group model and realtion to group in message

--- server/src/models/message.ts
+++ server/src/models/message.ts
@@ -1,8 +1,13 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-  user: {type: String, required: true},
-  message: {type: String, required: true},
+  user: { type: String, required: true },
+  message: { type: String, required: true },
+  group: {
+    type: Types.ObjectId,
+    ref: "Group",
+    required: true,
+  },
   timestamp: { type: Date, default: Date.now },
 });
 

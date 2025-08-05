b318fb445648c2e62f2ddd065fc2163fe72c541b
raaflii <rafliwrzky@gmail.com>
2025-08-05 09:49:35 +0700

    update default value group schema

--- server/src/models/group.ts
+++ server/src/models/group.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  createdBy: { type: String },
+  createdBy: { type: String, default: "SYS" },
   createdAt: { type: Date, default: Date.now },
 });
 
